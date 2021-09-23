d158bba6c4736e5ba17282598e51d2b3aa3ce822
Alex Garcia <alexandrogarcia@live.com>
2021-09-22 17:45:58 -0700

    Added deployement requirements - 112

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$9^cm5#^&*j&$0q__o@bpg7c0*$!%#t&c37og@p(4y()dcsnod
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
 
 
 # Application definition
